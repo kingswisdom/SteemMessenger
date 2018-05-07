dadf9234d7ba35291a8aa9dd9eb0307a1dd1d7ee
Kingswisdom <37601147+kingswisdom@users.noreply.github.com>
2018-05-07 07:09:57 +0200

    Updated serverdb.js for 0.0.4
    Modified aggregate function

--- serverdb.js
+++ serverdb.js
@@ -4,7 +4,7 @@ const url = process.env.MONGO_URI || "mongodb://127.0.0.1/steemMessenger";
 mongo.connect(url, function(err, db){
     if(err){
         console.log(err);
-    } 
+    }
     else {
     console.log('MongoDB connected...');
     }
@@ -14,30 +14,50 @@ var chat = db.collection('chats');
 
 exports.getMessages = function(data, limit, out){
 	var query = chat.find({tags: { $all: [data.user, data.receiver]}});
-	query.limit(limit).sort({_id:1}).toArray(function(err, res){
+	query.limit(limit).sort({timestamp:1}).toArray(function(err, res){
+		console.log
     	out(err, res);
 	});
 }
 
 exports.getLastMessage = function(data, out){
 	var query = chat.find({tags: { $all: [data.user, data.receiver]}});
-	query.limit(1).sort({_id:-1}).toArray(function(err, res){
+	query.limit(1).sort({timestamp:-1}).toArray(function(err, res){
 		out(err, res);
 	});
 }
 
+exports.getLatestMessages = function(data, out){
+    chat.aggregate([
+    	{$match: 
+    		{from: data.user}
+    	},
+    	{$sort: {
+    		_id:-1
+    	}},
+    	{$group: {
+    		_id: "$to",
+    		from: {$first: "$from"},
+    		message: {$first: "$message"},
+    		timestamp: { $first: "$timestamp" }
+    	}}
+    ]).toArray(function(err, res){
+        out(err, res);
+    });
+}
+
 exports.saveMessage = function(data){
-	console.log(data);
 	chat.insert({
-		"tags": [data.name, data.to], 
-		"author": data.name, 
+		"from": data.name,
+		"to": data.to,
+		"tags": [data.name, data.to],
 		"message": data.message,
 		"timestamp": Date.now()
 	});
 }
 
 exports.deleteDiscussion = function(data){
-	chat.remove({tags: { $all: [data.name, data.to]}});
+	chat.remove({from:data.name, to:data.to});
 }
 
 });
