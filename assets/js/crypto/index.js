487bccd4c72c9dfd4390cc1291f881c9a79a2b0f
cryptohazard <jyezie@yahoo.fr>
2018-06-20 09:14:00 +0200

    add crypto function for server auth and hmac derivation

--- assets/js/crypto/index.js
+++ assets/js/crypto/index.js
@@ -11,17 +11,21 @@ var Crypto = {};
 Crypto.generate_session_keys = function (private_wif, public_key){
   var private_key = KeyPrivate.fromWif(private_wif);
   var shared_secret = private_key.get_shared_secret(public_key);
-  var authenticationKey = base58.encode(Buffer.concat([Buffer.from("Authentication key"),shared_secret]));
-	var encryptionKey = base58.encode(Buffer.concat([Buffer.from("encryption key"),shared_secret]));
+	var authenticationKey = base58.encode(hash.HmacSHA256(shared_secret,"Authentication key"));
+	//TODO change message encryption from steem.auth.memo to AES-GCM
+	var encryptionKey = base58.encode(hash.HmacSHA256(shared_secret,"Encryption key"));
 	console.log("end crypto func");
   return {"authenticationKey": authenticationKey, "encryptionKey": encryptionKey};
 };
-/*
-var Crypto = {};
-Crypto.generate_session_keys = function (private_wif, public_key){
-	var matrix = "you are in the matrix";
-	console.log(matrix);
-	return matrix;
-}*/
+
+Crypto.authentication_token = function (authenticationKey){
+//TODO derive a session token from authenticationKey and date/hour or smth else
+	return authenticationKey;
+}
+Crypto.verify_client_authentication = function (receivedtoken, authenticationKey){
+	//TODO modify this when previous function is modified
+	var computedToken = this.authentication_token(authenticationKey);
+	return (computedToken == receivedtoken);
+}
 
 module.exports = Crypto;
