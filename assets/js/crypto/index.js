e3d323480ed0dc46f24aaf1bcd8990ec38a7954c
cryptohazard <jyezie@yahoo.fr>
2018-06-25 17:13:40 +0200

    new crypto functions

--- assets/js/crypto/index.js
+++ assets/js/crypto/index.js
@@ -1,4 +1,3 @@
-
 var Signature = require('./steem-js/src/auth/ecc/src/signature'),
 	KeyPrivate = require('./steem-js/src/auth/ecc/src/key_private'),
 	PublicKey = require('./steem-js/src/auth/ecc/src/key_public'),
@@ -10,10 +9,27 @@ var Crypto = {};
 
 
 Crypto.generate_session_keys = function (private_wif, public_key){
+	//console.log(private_wif);
   var private_key = KeyPrivate.fromWif(private_wif);
-  //var shared_secret = bs58.encode(private_key.get_shared_secret(public_key));
   var shared_secret = private_key.get_shared_secret(public_key);
-  var buf = Buffer.concat([Buffer.from("Authentication key"),shared_secret]);
-  return buf;
+	//console.log(base58.encode(shared_secret));
+	var authenticationKey = base58.encode(hash.HmacSHA256(shared_secret,"Authentication key"));
+	//TODO change message encryption from steem.auth.memo to AES-GCM
+	var encryptionKey = base58.encode(hash.HmacSHA256(shared_secret,"Encryption key"));
+	//console.log(authenticationKey);
+	//console.log(encryptionKey);
+	console.log("end crypto func");
+  return {"authenticationKey": authenticationKey, "encryptionKey": encryptionKey};
+};
+
+Crypto.authentication_token = function (authenticationKey){
+//TODO derive a session token from authenticationKey and date/hour or smth else
+	return authenticationKey;
 }
+Crypto.verify_client_authentication = function (receivedtoken, authenticationKey){
+	//TODO modify this when previous function is modified
+	var computedToken = this.authentication_token(authenticationKey);
+	return (computedToken == receivedtoken);
+}
+
 module.exports = Crypto;
