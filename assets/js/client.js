5d121b769504ad90973a82abc567a2bd5837887c
Kingswisdom <37601147+kingswisdom@users.noreply.github.com>
2018-07-02 07:17:05 +0200

    Created a passphrase login
    All the data needed for a user account to work with our service is now stored in an encrypted state in the localStorage. I added a passphrase selector screen, and a passphrase login screen to unlock the storage and access your encryption key, authentication token, etc...
    
    The code was also cleared a little by integrating the UI.js module.

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,6 +1,7 @@
 const steem = require("steem");
 var io = require('socket.io-client');
 var SM = require('./SM.js');
+var UI = require('./UI.js')
 
 var element = function(id){
 	return document.getElementById(id);
@@ -11,18 +12,28 @@ var messages = element('messages');
 var textarea = element('textarea');
 var username = element('username');
 var privMemoKey = element('privMemoKey');
+var newPassphrase = element('newPassphrase')
+var newPassphrase2 = element('newPassphrase2');
+var passphraseLoginInterface = element('passphraseLoginInterface');
+var passphraseUsername = element('passphraseUsername');
+var passphrase = element('passphrase');
+var passphraseLoginBtn = element('PassphraseLoginBtn');
 var receiver = element('receiver');
 var receiverInfo = element("receiver-info");
 var clearBtn = element('clear');
 var loginBtn = element('login');
 var friendBtn = element('friend');
 var startBtn = element('start');
+var passphraseSelectorScreen = element('passphraseSelectorScreen');
+var createPassphraseBtn = element("createPassphraseBtn");
 var logsucc = element("login-success");
 var splash = element("splash");
 var loader0 = element("loaderEffect0");
 var loader1 = element("loaderEffect1");
 var loader2 = element("loaderEffect2");
 var loader3 = element("loaderEffect3");
+var loader4 = element("loaderEffect4");
+var loader5 = element("loaderEffect5");
 var loginter = element("login-interface");
 var chatCont = element("chat-container");
 var fileSend = element('fileSend');
@@ -31,6 +42,8 @@ var emojiContainer = element("emoji-container");
 var previousDiscussions = element("previousDiscussions");
 emojiContainer.style.display = "none";
 DaChat.style.display = "none";
+passphraseSelectorScreen.style.display = "none";
+passphraseLoginInterface.style.display = "none";
 loginter.style.display = "none";
 chatCont.style.display = "none";
 logsucc.style.display = "none";
@@ -38,11 +51,14 @@ loader0.style.display = "none";
 loader1.style.display = "none";
 loader2.style.display = "none";
 loader3.style.display = "none";
+loader4.style.display = "none";
+loader5.style.display = "none";
 receiverInfo.style.display = "none";
 
 
 var user;
-var key; //TODO clean up all the places we send the private key to the server
+var key;
+var cookieB;
 var recipient;
 var socket = io.connect();
 var keys;
@@ -52,177 +68,154 @@ if(socket !== undefined){
 	console.log('Connection to server successful!');
 
 
-startBtn.addEventListener('click', function(){
-	if(document.cookie != ""){
-		startBtn.style.display = "none";
-		loader0.style.display = "block";
-		var cookieM = document.cookie;
-		var cookieB = JSON.parse(cookieM);
-		console.log(keys);
-		user = cookieB.user;
-		key = cookieB.privateKey;
-		SM.login({user:user, privWif:key}, function(out){
-			user = out.user;
-			keys = {uniquePublic:cookieB.uniquePublic, uniquePrivate:cookieB.uniquePrivate, authenticationKey:cookieB.authenticationKey, encryptionKey: cookieB.encryptionKey};
-			socket.emit('reinitialize', {encodedmsg: out.encodedmsg});
-			splash.style.display = "none";
-			logsucc.style.display = "block";
-			loader0.style.display = "none";
-			startBtn.style.display = "block";
-		});
-
-	}
-	else{
-		splash.style.display = "none";
-		loginter.style.display = "block";
-	}
-
-});
-
-loginBtn.addEventListener('click', function() {
-	SM.login({user:username.value, privWif:privMemoKey.value}, function(out){
-		user = out.user;
-		key = out.key;
-		return socket.emit('initialize', {encodedmsg: out.encodedmsg});
+	startBtn.addEventListener('click', function(){
+		SM.checkIfAlreadyConnected();
 	});
-});
 
-receiver.addEventListener('keydown', function(event){
-	if(event.which === 13 && event.shiftKey == false){
-		if(receiver.value != "") {
-			SM.chooseFriend({to:receiver.value}, function(out){
-				recipient = out.receiver;
-				receiver.value = "";
-				return socket.emit('fetchDiscussion', {name:user,to:recipient});
-			});
+	loginBtn.addEventListener('click', function() {
+		SM.login({user:username.value, privWif:privMemoKey.value}, function(result){
+			user = result.user;
+			key = result.key;
+			return socket.emit('initialize', {encodedmsg: result.encodedmsg});
+		});
+	});
+
+	createPassphraseBtn.addEventListener('click', function(){
+		SM.createWalletPassphrase({pass1: newPassphrase.value, pass2: newPassphrase2.value}, function(result){
+			if(result == "unmatching passphrases"){
+				UI.onPassphraseUnmatchShowError();
+			}
+			if(result == "ok"){
+				SM.initializeKeys({user: user, key: key, passphrase: newPassphrase.value}, function(out){
+					keys = {uniquePublic:out.uniquePublic, uniquePrivate:out.uniquePrivate, authenticationKey:out.authenticationKey, encryptionKey: out.encryptionKey};
+					UI.onNewPassphraseShowSuccessScreen(); 
+				});
+			}
+		})
+	});
+
+	passphraseLoginBtn.addEventListener('click', function() {
+		SM.passphraseLogin({user:passphraseUsername.value, passphrase:passphrase.value}, function(result){
+			var wallet = result.wallet;
+			var encodedContainer = result.encodedContainer;
+			user = wallet.user;
+	        key = wallet.privateKey;
+	        keys = {uniquePublic:wallet.uniquePublic, uniquePrivate:wallet.uniquePrivate};
+	        socket.emit('reinitialize', {encodedmsg: encodedContainer});
+	        UI.onValidPassphraseShowLoginSuccessScreen();
+		});
+	});
+
+	receiver.addEventListener('keydown', function(event){
+		if(event.which === 13 && event.shiftKey == false){
+			if(receiver.value != "") {
+				SM.chooseFriend({to:receiver.value}, function(out){
+					recipient = out.receiver;
+					receiver.value = "";
+					return socket.emit('fetchDiscussion', {name:user,to:recipient});
+				});
+			}
 		}
-	}
-});
+	});
 
-socket.on('logged', function(){
-	SM.initializeKeys({key: key}, function(out){
-		document.cookie = '{"user":"' + user + '","privateKey":"' + key + '","uniquePrivate":"' + out.uniquePrivate + '","uniquePublic":"' + out.uniquePublic + '","authenticationKey":"' + out.authenticationKey + '","encryptionKey":"' + out.encryptionKey + '"}';
-		keys = {uniquePublic:out.uniquePublic, uniquePrivate:out.uniquePrivate, authenticationKey:out.authenticationKey, encryptionKey: out.encryptionKey};
-		loader1.style.display = "none";
-		loginter.style.display = "none";
-		logsucc.style.display = "block";
+	socket.on('logged', function(){
+		UI.onFirstLoginShowPassphraseSelectorScreen(); 
 	})
 
-})
-
-socket.on('latest discussions', function(data){
-	console.log("getting latest discussions");
-	console.log(user);
-	if(key !== undefined) {
-		if(data.length) {
-			console.log("data is valid");
-			return SM.appendDiscussions(data, {id:user,key:keys.uniquePrivate});
+	socket.on('latest discussions', function(data){
+		if(key !== undefined) {
+			if(data.length) {
+				return SM.appendDiscussions(data, {id:user, key:key, uniqueKey:keys.uniquePrivate});
+			}
 		}
-	}
-});
+	});
 
-socket.on('output', function(data){
-	if(key !== undefined) {
-		if(data.length) {
-			console.log("data is valid");
-			return SM.appendMessages(data, {id:user,key:key,uniqueKey:keys.uniquePrivate,receiver:recipient});
+	socket.on('output', function(data){
+		if(key !== undefined) {
+			if(data.length) {
+				return SM.appendMessages(data, {id:user, key:key, uniqueKey:keys.uniquePrivate, receiver:recipient});
+			}
 		}
-	}
-});
+	});
 
-socket.on('file output', function(data){
-	if(key !== undefined) {
-			return SM.appendFile(data, {id:user,key:key,receiver:recipient});
-	}
-});
+	socket.on('file output', function(data){
+		if(key !== undefined) {
+				return SM.appendFile(data, {id:user,key:key,receiver:recipient});
+		}
+	});
 
-textarea.addEventListener('keydown', function(event){
-	if(event.which === 13 && event.shiftKey == false){
-		if(textarea.value != "") {
-			console.log("Handling input");
-			SM.handleInput({user:user,receiver:recipient,key:key,uniquePrivate:keys.uniquePrivate,message:textarea.value}, function(out){
-				encoded = out.encodedmsg;
-				console.log("Input has been processed !");
-				return socket.emit('input', {
-					name:user,
-					to:recipient,
-					message:encoded
+	textarea.addEventListener('keydown', function(event){
+		if(event.which === 13 && event.shiftKey == false){
+			if(textarea.value != "") {
+				SM.handleInput({user:user,receiver:recipient,key:key,uniquePrivate:keys.uniquePrivate,message:textarea.value}, function(out){
+					encoded = out.encodedmsg;
+					return socket.emit('input', {
+						name:user,
+						to:recipient,
+						message:encoded
+					});
 				});
-				console.log("Input has been sent to server !");
-			});
+			}
 		}
-	}
-});
-
-
-
-fileSend.addEventListener("change", function () {
-	console.log(this.files[0].size);
-	if(this.files[0].size > 100000){
-		this.value = "";
-        return alert("File is too big!");
-    }
-    else {
-		var file = this.files[0];
-		var reader = new FileReader();
-		reader.onloadend = function() {
-    		//console.log('RESULT', reader.result)
-    		SM.handleFile({user:user, receiver:recipient, key:key, file:reader.result}, function(out){
-    			encoded = out.encodedfile;
-    			loader3.style.display = "none";
-				textarea.style.display = "block";
-				textarea.value = "";
-				textarea.focus();
-    			return socket.emit('file input', {
-    				name:user,
-    				to:recipient,
-    				file:encoded,
-    			});
-
-    		});
-    	}
-    	reader.readAsDataURL(file);
-    }
-});
-
-
-emojiList.addEventListener("click",function(e) {
-	if(e.target && e.target.nodeName == "LI") {
-		textarea.value = textarea.value + " " + e.target.innerHTML;
-	}
-});
-
-exports.fetchDiscussion = function(data){
-	SM.chooseFriend({to:data.receiver}, function(out){
-		recipient = out.receiver;
-		return socket.emit('fetchDiscussion', {name:user,to:recipient});
 	});
-}
 
-returnToSelection.addEventListener('click', function(){
-	previousDiscussions.innerHTML = "";
-	socket.emit('fetchDiscussions', {name:user});
-	chatCont.style.display = "none";
-	receiverInfo.style.display = "none";
-	logsucc.style.display = "block";
-	loader3.style.display = "none";
-	textarea.style.display = "block";
-	textarea.value = "";
-	messages.innerHTML = "";
-	document.getElementById('receiverpicture').remove();
-	document.getElementById('receiverInf').remove();
-	var recipient = undefined;
-});
 
-clearBtn.addEventListener('click', function(){
-	messages.innerHTML = "";
-	return socket.emit('clear', {
-		name:user,
-		to:recipient
+
+	fileSend.addEventListener("change", function () {
+		console.log(this.files[0].size);
+		if(this.files[0].size > 100000){
+			this.value = "";
+	        return alert("File is too big!");
+	    }
+	    else {
+			var file = this.files[0];
+			var reader = new FileReader();
+			reader.onloadend = function() {
+	    		//console.log('RESULT', reader.result)
+	    		SM.handleFile({user:user, receiver:recipient, key:key, file:reader.result}, function(out){
+	    			encoded = out.encodedfile;
+	    			return socket.emit('file input', {
+	    				name:user,
+	    				to:recipient,
+	    				file:encoded,
+	    			});
+
+	    		});
+	    	}
+	    	reader.readAsDataURL(file);
+	    }
 	});
-});
 
-socket.on('cleared', function(){
-	messages.innerHTML = "";
-});
+
+	emojiList.addEventListener("click",function(e) {
+		if(e.target && e.target.nodeName == "LI") {
+			textarea.value = textarea.value + " " + e.target.innerHTML;
+		}
+	});
+
+	exports.fetchDiscussion = function(data){
+		SM.chooseFriend({to:data.receiver}, function(out){
+			recipient = out.receiver;
+			return socket.emit('fetchDiscussion', {name:user,to:recipient});
+		});
+	}
+
+	returnToSelection.addEventListener('click', function(){
+		socket.emit('fetchDiscussions', {name:user});
+		UI.returnToPreviousDiscussions();
+		recipient = undefined;
+	});
+
+	clearBtn.addEventListener('click', function(){
+		messages.innerHTML = "";
+		return socket.emit('clear', {
+			name:user,
+			to:recipient
+		});
+	});
+
+	socket.on('cleared', function(){
+		messages.innerHTML = "";
+	});
+
 }
