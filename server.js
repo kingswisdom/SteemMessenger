254dbe232c95495b03a4f575585a181da3af6cb6
Kingswisdom <37601147+kingswisdom@users.noreply.github.com>
2018-08-10 01:10:10 +0200

    Update server.js
    Now, the server start the "stream blockchain" operation in the STMServer.js. (`STMServer.start()`)
    
    This calls the new STMServer.js module
    
    
    Note the addition of the API Server mentions. Those will be deployed after further testing, maybe in 0.0.7

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const app = require('express')();
 const express = require('express');
 const path = require('path');
-const serverlib = require('./serverlib.js')
+const serverLib = require('./serverlib.js');
+//const apiServerLib = require('./apiServerLib.js')
+const STMServer = require('./STMServer.js');
 const ejs = require('ejs');
 const PORT = process.env.PORT || 4000;
+//const apiPORT = 3000;
 
-console.log(PORT);
+
 
 const server = app.listen(PORT);
-serverlib.listen(server);
+serverLib.listen(server);
+
+/*const apiServer = app.listen(apiPORT);
+apiServerLib.listen(server);*/ //TODO : Create apiServerLib.js
+
+
+STMServer.start();
 
 app.use(express.static(path.join(__dirname, './assets')));
 app.set('view engine', 'ejs');
