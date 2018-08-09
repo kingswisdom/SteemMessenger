726ab2e25c912293fe4ef9bd86d7fc297eeebf88
Kingswisdom <37601147+kingswisdom@users.noreply.github.com>
2018-08-10 01:05:51 +0200

    Create STMServer.js
    This module streams the blockchain, and search for incoming transaction/vote to the steem-messenger account/last steem-messenger post.
    
    This script will also save all the users that send a transaction/vote into the database (`db.setSubscription`). That way, we can now check on the server side if a given user has a paid plan or not.

---
+++ STMServer.js
@@ -0,0 +1,56 @@
+const db = require('./serverdb.js');
+const steem = require("steem");
+
+const owner = "steem-messenger";
+const memoError = "Invalid amount : Send 1 or 5 SBD to complete your subscription. Please refer to our website for pricing";
+
+exports.start = function(){
+	streamBlockchain();
+}:
+
+function streamBlockchain(){
+	steem.api.streamOperations("head", function(err, result) {
+		try {
+			if(result.length){
+		        if (result[0] == 'transfer') {
+		            var memoContent = result[1].memo;
+					var sender = result[1].from;
+					var account = result[1].to;
+					var amount = result[1].amount;
+					if (account == "steem-messenger"){
+						if (amount == "1.000 SBD"){
+							db.setSubscription({name: sender, plan: 1});
+						}
+						/*if (amount == "5.000 SBD"){
+							db.setSubscription({name: sender, plan: 2}); amount
+						}*/
+						else {
+							steem.broadcast.transfer(SMActivePrivate, owner, sender, amount, memoError, function(err, res) {
+		                        console.log(err, res);
+		                    });
+						}
+					}
+		        }
+		        if (result[0] == 'vote') {
+		        	var voter = result[1].voter;
+					var author = result[1].author;
+					var permlink = result[1].permlink;
+					var weight = result[1].weight;
+					if (author == "steem-messenger"){
+						steem.api.getDiscussionsByBlog({tag: author, limit: 1}, function(err, result){
+							if (permlink == result[0].permlink){
+								if(weight == 10000){
+									db.setSubscription({name: voter, plan: 0});
+								}
+							}
+						});
+					}
+		        }
+	    	}
+	    }    
+	    catch(err) {
+	    	console.log(err);
+	    	process.exit(1);
+	    }
+	});	
+};
