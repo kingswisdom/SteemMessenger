97c1b6ed9e6cb0162b7eab22aa30156b9771fc6c
Kingswisdom <37601147+kingswisdom@users.noreply.github.com>
2018-08-12 22:58:18 +0200

    Update Lara.js
    Now, Lara checks if the user is subscribed to the Steem Messenger Services, and reacts accordingly

--- Lara.js
+++ Lara.js
@@ -25,8 +25,9 @@ exports.checkLogin = function(data, out){
 				out(undefined);
 			}
 		}
-	});
+	})
 }
+
 
 exports.checkIdentity = function(data, out){
 	rawContainer = steem.memo.decode(LaraPrivateKey, data.message);
@@ -35,19 +36,28 @@ exports.checkIdentity = function(data, out){
 	raw = raw.join("");
 	var decodedContainer = JSON.parse(raw);
 	steem.api.getAccounts([decodedContainer.user], function(err, result){
+
 		if(result.length > 0) {
-			pubWif = result[0]["memo_key"];
-			var sessionKeys = crypto.generate_session_keys(LaraPrivateKey, pubWif);
-			console.log(decodedContainer.token);
-			var isvalid = crypto.verify_client_authentication(decodedContainer.token, sessionKeys.authenticationKey);
-			if(isvalid) {
-				console.log('Lara : Identity confirmed ! The message will be processed securely to the recipient !')
-				out({name: decodedContainer.user, to: decodedContainer.to, message: decodedContainer.message});
-			}
-			else {
-				console.log("Lara : Someone just tried to forge a message as @"  + decodedContainer.user);
-				out(undefined);
-			}
+			db.checkSubscription({name: decodedContainer.user}, function(res){
+				console.log(decodedContainer.user)
+				if(res.length > 0 && res[0].name != undefined && res[0].end >= Date.now()){
+					pubWif = result[0]["memo_key"];
+					var sessionKeys = crypto.generate_session_keys(LaraPrivateKey, pubWif);
+					console.log(decodedContainer.token);
+					var isvalid = crypto.verify_client_authentication(decodedContainer.token, sessionKeys.authenticationKey);
+					if(isvalid) {
+						console.log('Lara : Identity confirmed ! The message will be processed securely to the recipient !')
+						out({name: decodedContainer.user, to: decodedContainer.to, message: decodedContainer.message});
+					}
+					else {
+						console.log("Lara : Someone just tried to forge a message as @"  + decodedContainer.user);
+						out(undefined);
+					}
+				}
+				else{
+					out({error: "not subscribed"});
+				}
+			});
 		}
 	});
 };
