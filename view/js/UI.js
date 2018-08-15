7559c533972a2f0530b350f39776da1b5e6cf485
Kingswisdom <37601147+kingswisdom@users.noreply.github.com>
2018-08-15 02:56:45 +0200

    Create UI.js

---
+++ view/js/UI.js
@@ -0,0 +1,82 @@
+var UI = require('./UIlib.js')
+
+
+
+exports.onNotConnectedShowLoginInterface = function(){
+	UI.hideSplash();
+    UI.showLoginInterface();
+}
+
+exports.onFirstLoginShowPassphraseSelectorScreen = function(){
+	UI.hideLoader1();
+	UI.hideLoginInterface();
+	UI.showPassphraseSelectorScreen();
+}
+
+exports.onNewPassphraseShowSuccessScreen = function(){
+	UI.hideLoader4();
+    UI.showPassphraseLoginBtn();
+	UI.hidePassphraseSelectorScreen();
+	UI.showLoginSuccess();
+}
+
+exports.onConnectedShowPassphraseLoginInterface = function(){
+    UI.hideSplash();
+    UI.showPassphraseLoginInterface();
+    UI.hideLoader0();
+    UI.showStartButton();	
+}
+
+exports.onPassphraseUnmatchShowError = function(){
+    UI.hideLoader4();
+    UI.showPassphraseLoginBtn();
+    UI.showErrorPassphraseLoginBtn();	
+}
+
+exports.onValidPassphraseShowLoginSuccessScreen = function(){
+    UI.hidePassphraseLoginInterface();
+    UI.showLoginSuccess();
+    UI.hideLoader5();
+    UI.showPassphraseLoginBtn();
+}
+
+exports.returnToPreviousDiscussions = function(){
+    UI.hideNotSubscribed();
+    UI.clearPreviousDiscussions();
+	UI.hideChatContainer();
+	UI.hideReceiverInfo();
+	UI.showLoginSuccess();
+	UI.hideLoader3();
+	UI.showChatTextInputArea();
+	UI.clearChatTextInputArea();
+	UI.clearMessages();
+	UI.clearReceiverPicture();
+	UI.clearReceiverInf();
+}
+
+exports.onNotSubscribed = function(){
+	UI.showNotSubscribed();
+}
+
+exports.onSendHideChatTextArea = function(){
+    UI.hideChatTextInputArea();
+	UI.showLoader3();
+}
+
+exports.onSentShowChatTextArea = function(){
+    UI.showChatTextInputArea();
+	UI.hideLoader3();
+	UI.clearChatTextInputArea();
+	UI.focusChatTextInputArea();
+}
+
+exports.showWhoIsWriting = function(data){
+	UI.isWritingValue(data);
+	UI.showIsWriting();
+}
+
+exports.hideWhoIsWriting = function(){
+	UI.isWritingClearValue();
+	UI.hideIsWriting();
+}
+
