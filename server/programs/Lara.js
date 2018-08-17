112780448d134b8a3272b9b4ef1655e5e01f2797
Kingswisdom <37601147+kingswisdom@users.noreply.github.com>
2018-08-17 16:17:23 +0200

    Update Lara.js
    Added `decodeSafeSocket` and `encodeSafeSocket`, which are part of the redesign of the code logic

--- server/programs/Lara.js
+++ server/programs/Lara.js
@@ -18,7 +18,7 @@ exports.checkLogin = function(data, out){
 			var isvalid = crypto.verify_client_authentication(decodedContainer.token, sessionKeys.authenticationKey);
 			if(isvalid) {
 				console.log('Lara : Connexion approved !')
-				out({name: decodedContainer.user});
+				out({user: decodedContainer.user});
 			}
 			else {
 				console.log("Lara : Someone  just tried to bypass authentication check as @" + decodedContainer.user);
@@ -37,16 +37,16 @@ exports.checkIdentity = function(data, out){
 	var decodedContainer = JSON.parse(raw);
 	steem.api.getAccounts([decodedContainer.user], function(err, result){
 		if(result.length > 0) {
-			db.checkSubscription({name: decodedContainer.user}, function(res){
+			db.checkSubscription({user: decodedContainer.user}, function(res){
 				console.log(decodedContainer.user)
-				if(res.length > 0 && res[0].name != undefined && res[0].end >= Date.now()){
+				if(res.length > 0 && res[0].user != undefined && res[0].end >= Date.now()){
 					pubWif = result[0]["memo_key"];
 					var sessionKeys = crypto.generate_session_keys(LaraPrivateKey, pubWif);
 					console.log(decodedContainer.token);
 					var isvalid = crypto.verify_client_authentication(decodedContainer.token, sessionKeys.authenticationKey);
 					if(isvalid) {
 						console.log('Lara : Identity confirmed ! The message will be processed securely to the recipient !')
-						out({name: decodedContainer.user, to: decodedContainer.to, message: decodedContainer.message});
+						out({user: decodedContainer.user, to: decodedContainer.to, message: decodedContainer.message});
 					}
 					else {
 						console.log("Lara : Someone just tried to forge a message as @"  + decodedContainer.user);
@@ -61,8 +61,8 @@ exports.checkIdentity = function(data, out){
 	});
 };
 
-exports.processRequest = function(data, out){
-	rawContainer = steem.memo.decode(LaraPrivateKey, data.message);
+exports.decodeSafeSocket = function(data, out){
+	rawContainer = steem.memo.decode(LaraPrivateKey, data.encodedmsg);
 	raw = rawContainer.split("");
 	raw.shift();
 	raw = raw.join("");
@@ -75,12 +75,25 @@ exports.processRequest = function(data, out){
 			var isvalid = crypto.verify_client_authentication(decodedContainer.token, sessionKeys.authenticationKey);
 			if(isvalid) {
 				console.log('Lara : Identity confirmed ! The request will be processed securely to the recipient !')
-				out({name: decodedContainer.user, to: decodedContainer.to});
+				out(decodedContainer);
 			}
 			else {
 				console.log("Lara : Someone just tried to forge a request as @"  + decodedContainer.user);
 				out(undefined);
 			}
+		}
+	});
+}
+
+exports.encodeSafeSocket = function(data, out){
+	steem.api.getAccounts([data.identity], function(err, result){
+		if(result.length > 0) {
+			var pubWif = result[0]["memo_key"];
+			var sessionKeys = crypto.generate_session_keys(LaraPrivateKey, pubWif);
+            data.token = crypto.authentication_token(sessionKeys.authenticationKey);
+            var Container = "#" + JSON.stringify(data);
+            var encodedContainer = steem.memo.encode(LaraPrivateKey, pubWif, Container);
+            out({encodedmsg: encodedContainer});
 		}
 	});
 }
