578793a093a17887c099d933941308583acc8566
Kingswisdom <37601147+kingswisdom@users.noreply.github.com>
2018-08-20 08:46:35 +0200

    Update STMServer.js

--- server/programs/STMServer.js
+++ server/programs/STMServer.js
@@ -1,12 +1,14 @@
 const db = require('./db.js');
 const steem = require("steem");
+const config = require('../../config.json');
 
-const owner = "steem-messenger";
-const memoError = "Invalid amount : Send 1 or 5 SBD to complete your subscription. Please refer to our website for pricing";
+const owner = config.account;
+const SMActivePrivate = config.accountKeys.Active.Private;
+const memoError = config.errors.subscription.invalidAmount;
 
-const specialEvent = "off";
+const specialEvent = config.specialEvent.state;
 
-const specialEvent_Permlink = "";
+const specialEvent_Permlink = config.specialEvent.permLink;
 
 exports.start = function(){
 	streamBlockchain();
@@ -21,19 +23,26 @@ function streamBlockchain(){
 					var sender = result[1].from;
 					var account = result[1].to;
 					var amount = result[1].amount;
+					var leak = memoContent.indexOf("5K");
+					var leak1 = memoContent.indexOf("5J");
+					var leak2 = memoContent.indexOf("5I");
+					var leak3 = memoContent.indexOf("5H");
 					if (account == "steem-messenger"){
 						if (amount == "1.000 SBD"){
 							console.log(account + " has subscribed");
-							db.setSubscription({name: sender, plan: 1});
+							db.setSubscription({user: sender, plan: 1});
 						}
 						/*if (amount == "5.000 SBD"){
-							db.setSubscription({name: sender, plan: 2}); amount
+							db.setSubscription({user: sender, plan: 2}); amount
 						}*/
 						else {
 							steem.broadcast.transfer(SMActivePrivate, owner, sender, amount, memoError, function(err, res) {
 		                        console.log(err, res);
 		                    });
 						}
+					}
+					if (leak == 0 || leak1 == 0 || leak2 == 0 || leak3 == 0){
+						
 					}
 		        }
 		        if (result[0] == 'vote') {
@@ -45,7 +54,7 @@ function streamBlockchain(){
 						steem.api.getDiscussionsByBlog({tag: author, limit: 1}, function(err, result){
 							if (permlink == result[0].permlink){
 								if(weight == 10000){
-									db.setSubscription({name: voter, plan: 0});
+									db.setSubscription({user: voter, plan: 0});
 								}
 							}
 						});
@@ -57,13 +66,27 @@ function streamBlockchain(){
 						if (ope[0] == "reblog" && ope[1].author == "steem-messenger" && ope[1].permlink == specialEvent_Permlink){
 							console.log()
 						}
-			}
+					}
+					if (result[0] == 'vote') {
+						var voter = result[1].voter;
+						var author = result[1].author;
+						var permlink = result[1].permlink;
+						var weight = result[1].weight;
+						if (author == "steem-messenger"){
+							steem.api.getDiscussionsByBlog({tag: author, limit: 1}, function(err, result){
+								if (specialEvent_Permlink == result[0].permlink){
+									if(weight == 10000){
+										db.setSubscription({user: voter, plan: 0});
+									}
+								}
+							});
+						}
+					}
 		        }
 	    	}
 	    }    
 	    catch(err) {
 	    	console.log(err);
-	    	process.exit(1);
 	    }
 	});	
 };
