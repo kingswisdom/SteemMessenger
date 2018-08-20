ab62d7866c20c868e6a517d25451e1a35a2a8a15
Kingswisdom <37601147+kingswisdom@users.noreply.github.com>
2018-08-20 09:25:34 +0200

    Update storage.js
    Updated the scrypt parameters;
    
    Added a `generateSalt` function

--- view/js/storage.js
+++ view/js/storage.js
@@ -4,20 +4,25 @@ var scrypt = require('scryptsy');
 exports.createSafeStorage = function(data, passphrase){
 	var JSON_data = JSON.parse(data);
 	var user = JSON_data.user;
-	var salt = "SodiumChloride"
-	var kdfResult = scrypt(passphrase, salt, 16384, 8, 1, 64)
-	var encryptedWallet = CryptoJS.AES.encrypt(data, kdfResult.toString('hex')).toString();
-	localStorage.setItem(user, encryptedWallet);
+	generateSalt(user, function(out){
+		var salt = out.salt;
+		var kdfResult = scrypt(passphrase, salt, 65536, 8, 2, 64)
+		var encryptedWallet = CryptoJS.AES.encrypt(data, kdfResult.toString('hex')).toString();
+		localStorage.setItem(user, encryptedWallet);		
+	});
+	
 }
 
 exports.readSafeStorage = function(data, wallet){
 	var encryptedWallet = localStorage.getItem(data.user);
 	try{
-		var salt = "SodiumChloride"
-		var kdfResult = scrypt(data.passphrase, salt, 16384, 8, 1, 64)
-		var decryptedWallet = CryptoJS.AES.decrypt(encryptedWallet, kdfResult.toString('hex')).toString(CryptoJS.enc.Utf8);	
-		var JSON_wallet = JSON.parse(decryptedWallet);
-		wallet(JSON_wallet);
+		generateSalt(data.user, function(out){
+			var salt = out.salt;
+			var kdfResult = scrypt(data.passphrase, salt, 65536, 8, 2, 64)
+			var decryptedWallet = CryptoJS.AES.decrypt(encryptedWallet, kdfResult.toString('hex')).toString(CryptoJS.enc.Utf8);	
+			var JSON_wallet = JSON.parse(decryptedWallet);
+			wallet(JSON_wallet);
+		});
 	}
 	catch(err) {
     	wallet(err);
@@ -29,14 +34,22 @@ exports.updateSafeStorage = function(data, passphrase){
 	var JSON_data = JSON.parse(data);
 	var user = JSON_data.user;
  	localStorage.removeItem(user);
- 	var salt = "SodiumChloride"
-	var kdfResult = scrypt(passphrase, salt, 16384, 8, 1, 64)
-	var encryptedWallet = CryptoJS.AES.encrypt(data, kdfResult.toString('hex')).toString();
-	localStorage.setItem(user, encryptedWallet);
+ 	generateSalt(data.user, function(out){
+		var salt = out.salt;
+		var kdfResult = scrypt(passphrase, salt, 65536, 8, 2, 64)
+		var encryptedWallet = CryptoJS.AES.encrypt(data, kdfResult.toString('hex')).toString();
+		localStorage.setItem(user, encryptedWallet);
+	});
 }
 
 exports.deleteSafeStorage = function(data, passphrase){
 	var JSON_data = JSON.parse(data);
 	var user = JSON_data.user;
  	localStorage.removeItem(user);
+}
+
+function generateSalt = function(data, out){
+	var salt = "6CoeaohnQBMLYbQU3nkyfGqMeLG68n8MVnf5Zk2dzN2Bocdq43";
+	var kdfResult = scrypt(data.user, salt, 16384, 8, 1, 64);
+	out({salt: kdfResult});
 }
