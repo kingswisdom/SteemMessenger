de7ab4045beb789d2f737e1a1a1fff2b5da5456e
cryptohazard <jyezie@yahoo.fr>
2018-08-26 00:25:57 +0200

    complete benchmark, result + explanation

--- view/js/tests/scrypt_benchmark/README.md
+++ view/js/tests/scrypt_benchmark/README.md
@@ -1,6 +1,47 @@
 Benchmark and configuration for scrypt
 -------------------------------------
+We use https://github.com/bestiejs/benchmark.js/
+First download benchmark.js, platform.js and lodash.js.
+Second bundle the benchmmark js for the browser:
+
 ```
 browserify tests.js -o bundle.js
 ```
-First download benchmark.js, platform.js and loadash.js.
+
+Now fire ```index.html``` in your browser and look in the console for the result.
+
+## Some results
+Settings: VM with 2 cpus, 3GB of RAM running Ubuntu
+Using directly ```node tests.js``` (outside of the browser)
+```
+========================
+starting Benchmark
+========================
+default x 0.20 ops/sec ±7.02% (5 runs sampled) => average time 4.9214229959999996 seconds
+power16 x 0.10 ops/sec ±16.18% (5 runs sampled) => average time 10.213542985 seconds
+power17 x 0.05 ops/sec ±10.16% (5 runs sampled) => average time 20.036429998400003 seconds
+power18 x 0.05 ops/sec ±7.91% (5 runs sampled) => average time 20.8626994698 seconds
+Fastest is default
+
+```
+In Firefox I get:
+```
+========================
+starting Benchmark
+========================
+default x 0.30 ops/sec ±12.16% (5 runs sampled) => average time 3.3468000000000004 seconds
+power16 x 0.15 ops/sec ±3.72% (5 runs sampled) => average time 6.8134 seconds
+power17 x 0.07 ops/sec ±15.61% (5 runs sampled) => average time 13.978800000000001 seconds
+power18 x 0.08 ops/sec ±18.22% (5 runs sampled) => average time 12.216800000000001 seconds
+Fastest is default
+```
+
+## Scrypt Parameters explanation
+* scrypt article
+* ethereum wallet (2^18): https://ethereum.stackexchange.com/questions/37150/ethereum-wallet-v3-format
+* filippo explanation: https://blog.filippo.io/the-scrypt-parameters/
+* ethereum wallet cracker : https://stealthsploit.com/2018/01/04/ethereum-wallet-cracking-pt-2-gpu-vs-cpu/
+
+## Choice:
+I recommend ```N=2^16, r=8, p=1```. I would like to increase ```N``` to ```2^18```. An attacker will not use Javascript code to attack so we are only making her job easier if we can not wait a bit longer to use our keys.
+## Why do we need Scrypt?  A history of password cracking
--- view/js/tests/scrypt_benchmark/tests.js
+++ view/js/tests/scrypt_benchmark/tests.js
@@ -6,20 +6,74 @@ var Benchmark = require('./benchmark.js')
 
 var suite = new Benchmark.Suite;
 
-var salt = "comment ca va?";
-var passphrase = "hello world";
-suite.add('Default_scrypt',function(){
-    var kdfResult = scrypt(passphrase, salt, 16384, 8, 1, 64);
+var salt = "supercalifragilisticexpialidocious";
+//tests cases from scrypt.pdf
+var passphraseSimple = "hello world";
+var passphraseLower6 = "sfgroy";
+var passphraseLower8 = "ksuvnwyf";
+var passphrasePrint8 = "6,uh3y[a";
+var passphraseRand10 = "H.*W8Jz&r3";
+var passphraseString40 = "This is a 40-character string of English";
+var passphraseString80 = "This is an 80-character phrase which you probably won’t be able to crack easily.";
+
+// We expect the result to be used with AES-GCM
+// So the size key=32 bytes and IV = 12 bytes ==> 44 bytes for scrypt output
+
+console.log("========================\nstarting Benchmark \n========================")
+suite.add('default',function(){
+    var kdfResult = scrypt(passphraseString80, salt, Math.pow(2,15), 8, 1, 44);
     var result = kdfResult.toString('hex').toString();
   })
-  .add('Default_scrypt_lower',function(){
-      var kdfResult = scrypt(passphrase, salt, 16384, 8, 1, 44);
+  .add('power16',function(){
+      var kdfResult = scrypt(passphraseString80, salt, Math.pow(2,16), 8, 1, 44);
       var result = kdfResult.toString('hex').toString();
     })
-  .add('Default_scrypt_higher',function(){
-      var kdfResult = scrypt(passphrase, salt, 16384, 8, 1, 128);
+    .add('power17',function(){
+        var kdfResult = scrypt(passphraseString80, salt, Math.pow(2,17), 8, 1, 44);
+        var result = kdfResult.toString('hex').toString();
+      })
+      .add('power18',function(){
+          var kdfResult = scrypt(passphraseString80, salt, Math.pow(2,17), 8, 1, 44);
+          var result = kdfResult.toString('hex').toString();
+        })
+  .on('cycle', function(event) {
+    console.log(String(event.target)+" => average time "+ String(event.target.times.cycle) + " seconds");
+  })
+  .on('complete', function() {
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+  })
+  .run({
+    'async': true
+  });
+/*
+suite.add('simple',function(){
+    var kdfResult = scrypt(passphraseSimple, salt, 16384, 8, 1, 44);
+    var result = kdfResult.toString('hex').toString();
+  })
+  .add('lower 6 char',function(){
+      var kdfResult = scrypt(passphraseLower6, salt, 16384, 8, 1, 44);
       var result = kdfResult.toString('hex').toString();
     })
+    .add('lower 8 char',function(){
+        var kdfResult = scrypt(passphraseLower8, salt, 16384, 8, 1, 44);
+        var result = kdfResult.toString('hex').toString();
+      })
+      .add('printable  8 char',function(){
+          var kdfResult = scrypt(passphrasePrint8, salt, 16384, 8, 1, 44);
+          var result = kdfResult.toString('hex').toString();
+        })
+        .add('rand 10 char',function(){
+            var kdfResult = scrypt(passphraseRand10, salt, 16384, 8, 1, 44);
+            var result = kdfResult.toString('hex').toString();
+          })
+          .add('string 40 char',function(){
+              var kdfResult = scrypt(passphraseString40, salt, 16384, 8, 1, 44);
+              var result = kdfResult.toString('hex').toString();
+            })
+            .add('string 80 char',function(){
+                var kdfResult = scrypt(passphraseString80, salt, 16384, 8, 1, 44);
+                var result = kdfResult.toString('hex').toString();
+              })
   .on('cycle', function(event) {
     console.log(String(event.target));
   })
@@ -29,3 +83,4 @@ suite.add('Default_scrypt',function(){
   .run({
     'async': true
   });
+*/
