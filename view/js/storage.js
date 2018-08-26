2fab9088725837e54a32d07c9e9e5f7cebd5807c
cryptohazard <jyezie@yahoo.fr>
2018-08-26 02:05:54 +0200

    fix scrypt params and clean up salt generation

--- view/js/storage.js
+++ view/js/storage.js
@@ -1,38 +1,39 @@
 var CryptoJS = require("crypto-js");
 var scrypt = require('scryptsy');
+const secureRandom = require('secure-random');
 
 exports.createSafeStorage = function(data, passphrase){
 	var JSON_data = JSON.parse(data);
 	var user = JSON_data.user;
 	generateSalt({user:user}, function(out){
 		var salt = out.salt;
-		var params = {salt: salt, N: 65536, r: 8, p: 2, keyLen: 64};
+		var params = {salt: salt, N: 65536, r: 8, p: 1, keyLen: 44};
 		var kdfResult = scrypt(passphrase, salt, params.N, params.r, params.p, params.keyLen)
 		var encryptedWallet = CryptoJS.AES.encrypt(data, kdfResult.toString('hex')).toString();
 		var wallet = '{"encryptedWallet": "'+encryptedWallet+'", "params": {"salt":"'+params.salt+'", "N":'+params.N+', "r":'+params.r+', "p":'+params.p+', "keyLen":'+params.keyLen+'}}'
-		localStorage.setItem(user, wallet);	
-		console.log(localStorage)	
+		localStorage.setItem(user, wallet);
+		console.log(localStorage)
 	});
-	
+
 }
 
 exports.readSafeStorage = function(data, out){
 	var wallet = localStorage.getItem(data.user);
 	try{
-			
+
 			var ope = JSON.parse(wallet);
 			var params = ope.params;
 			var salt = params.salt;
 			console.log(ope)
 			var kdfResult = scrypt(data.passphrase, salt, params.N, params.r, params.p, params.keyLen)
-			var decryptedWallet = CryptoJS.AES.decrypt(ope.encryptedWallet, kdfResult.toString('hex')).toString(CryptoJS.enc.Utf8);	
+			var decryptedWallet = CryptoJS.AES.decrypt(ope.encryptedWallet, kdfResult.toString('hex')).toString(CryptoJS.enc.Utf8);
 			var JSON_wallet = JSON.parse(decryptedWallet);
 			out(JSON_wallet);
 	}
 	catch(err) {
     	out(err);
     }
-	
+
 }
 
 exports.updateSafeStorage = function(data, passphrase){
@@ -53,9 +54,10 @@ exports.deleteSafeStorage = function(data, passphrase){
 	var user = JSON_data.user;
  	localStorage.removeItem(user);
 }
-
+// return a buffer because pbkdf2sync used in scrypt can handle buffers
 function generateSalt(data, out){
-	var salt = "6CoeaohnQBMLYbQU3nkyfGqMeLG68n8MVnf5Zk2dzN2Bocdq43";
-	var kdfResult = scrypt(data.user, salt, 16384, 8, 1, 64).toString('hex');
-	out({salt: kdfResult});
+	var randBuffer = secureRandom.randomBuffer(32);
+	var userBuffer = Buffer.from(data.user);
+	var salt = Buffer.concat([randBuffer,userBuffer], userBuffer.length + randBuffer.length)
+	out({salt: salt});
 }
