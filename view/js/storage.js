a11ecf7310d5d17d334b89ced8a35852fa67f220
cryptohazard <jyezie@yahoo.fr>
2018-08-26 22:22:22 +0200

    improve scrypt salt formatting and remove storage update security flaw

--- view/js/storage.js
+++ view/js/storage.js
@@ -2,15 +2,15 @@ var CryptoJS = require("crypto-js");
 var scrypt = require('scryptsy');
 const secureRandom = require('secure-random');
 
+//TODO: check the encoding of encryptedWallet. 
 exports.createSafeStorage = function(data, passphrase){
 	var JSON_data = JSON.parse(data);
 	var user = JSON_data.user;
 	generateSalt({user:user}, function(out){
-		var salt = out.salt;
-		var params = {salt: salt, N: 65536, r: 8, p: 1, keyLen: 44};
-		var kdfResult = scrypt(passphrase, salt, params.N, params.r, params.p, params.keyLen)
+		var params = {salt: out.salt, N: 65536, r: 8, p: 1, keyLen: 44};
+		var kdfResult = scrypt(passphrase, params.salt, params.N, params.r, params.p, params.keyLen)
 		var encryptedWallet = CryptoJS.AES.encrypt(data, kdfResult.toString('hex')).toString();
-		var wallet = '{"encryptedWallet": "'+encryptedWallet+'", "params": {"salt":"'+params.salt+'", "N":'+params.N+', "r":'+params.r+', "p":'+params.p+', "keyLen":'+params.keyLen+'}}'
+		var wallet = '{"encryptedWallet": "'+encryptedWallet+'", "params": {"salt":"'+params.salt.toString('hex')+'", "N":'+params.N+', "r":'+params.r+', "p":'+params.p+', "keyLen":'+params.keyLen+'}}'
 		localStorage.setItem(user, wallet);
 		console.log(localStorage)
 	});
@@ -22,7 +22,7 @@ exports.readSafeStorage = function(data, out){
 	try{
 		var ope = JSON.parse(wallet);
 		var params = ope.params;
-		var salt = params.salt;
+		var salt = Buffer.from(params.salt, 'hex');
 		console.log(ope)
 		var kdfResult = scrypt(data.passphrase, salt, params.N, params.r, params.p, params.keyLen)
 		var decryptedWallet = CryptoJS.AES.decrypt(ope.encryptedWallet, kdfResult.toString('hex')).toString(CryptoJS.enc.Utf8);
@@ -36,16 +36,11 @@ exports.readSafeStorage = function(data, out){
 }
 
 exports.updateSafeStorage = function(data, passphrase){
-	var JSON_data = JSON.parse(data);
-	var user = JSON_data.user;
-	var wallet = localStorage.getItem(user);
-	var params = wallet.params;
-	var salt = params.salt;
-	var kdfResult = scrypt(passphrase, salt, params.N, params.r, params.p, params.keyLen)
-	var encryptedWallet = CryptoJS.AES.encrypt(data, kdfResult.toString('hex')).toString();
-	var wallet = JSON.stringify({encryptedWallet: encryptedWallet, params: params})
+	// previous version was insecured because the salt was the same between updates
+	// Question: do we actually need to removeitem? Check how storage value are updated.
+	// TODO: check where it serves us best to put the wallet
 	localStorage.removeItem(user);
-	localStorage.setItem(user, wallet);
+	createSafeStorage(data,passphrase);
 }
 
 exports.deleteSafeStorage = function(data, passphrase){
